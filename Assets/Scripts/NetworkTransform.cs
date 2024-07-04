d80f66698ce39b5926bc7dd412bb29b81d3255ac
jKnepel <knepel.julian@tutanota.com>
2024-07-04 23:44:21 +0200

    refactor: simplify transform synchro, let transform transmit data itself

--- Assets/Scripts/NetworkTransform.cs
+++ Assets/Scripts/NetworkTransform.cs
@@ -1,4 +1,5 @@
 using jKnepel.SimpleUnityNetworking.Managing;
+using jKnepel.SimpleUnityNetworking.Networking;
 using jKnepel.SimpleUnityNetworking.Serialising;
 using System;
 using UnityEngine;
@@ -11,6 +12,14 @@ namespace jKnepel.SynchronisationSchemes
     [AddComponentMenu("SimpleUnityNetworking/Component/Network Transform")]
     public class NetworkTransform : MonoBehaviour
     {
+        private enum ETransformFlag : byte
+        {
+            Position,
+            Rotation,
+            Scale
+        }
+        
+        [SerializeField] private ENetworkChannel _synchroniseChannel = ENetworkChannel.ReliableOrdered;
         [SerializeField] private bool _synchronisePosition = true;
         [SerializeField] private bool _synchroniseRotation = true;
         [SerializeField] private bool _synchroniseScale = true;
@@ -19,7 +28,7 @@ namespace jKnepel.SynchronisationSchemes
         private Vector3 _position;
         private Quaternion _rotation;
         private Vector3 _scale;
-
+        
         private string _transformNetworkID;
         private INetworkManager _syncNetworkManager;
 
@@ -30,80 +39,87 @@ namespace jKnepel.SynchronisationSchemes
             _networkObject.OnSyncNetworkManagerUpdated += NetworkIDUpdated;
             _transformNetworkID = $"{_networkObject.NetworkID}#Transform";
             _syncNetworkManager = _networkObject.SyncNetworkManager;
-            _syncNetworkManager?.Client.RegisterByteData(_transformNetworkID, TransformUpdateReceived);
+            if (_syncNetworkManager is not null)
+            {
+                _syncNetworkManager.Client.RegisterByteData(_transformNetworkID, TransformUpdateReceived);
+                _syncNetworkManager.OnTickStarted += CheckTransformUpdates;
+            }
             
             _position = transform.position;
             _rotation = transform.rotation;
             _scale = transform.localScale;
         }
 
-        private void Update()
+        private void NetworkIDUpdated()
         {
-            if (!transform.hasChanged) return;
-            transform.hasChanged = false;
-
+            if (_syncNetworkManager is not null)
+            {
+                _syncNetworkManager.Client.UnregisterByteData(_transformNetworkID, TransformUpdateReceived);
+                _syncNetworkManager.OnTickStarted -= CheckTransformUpdates;
+            }
+            _transformNetworkID = $"{_networkObject.NetworkID}#Transform";
+            _syncNetworkManager = _networkObject.SyncNetworkManager;
+            if (_syncNetworkManager is not null)
+            {
+                _syncNetworkManager.Client.RegisterByteData(_transformNetworkID, TransformUpdateReceived);
+                _syncNetworkManager.OnTickStarted += CheckTransformUpdates;
+            }
+        }
+        
+        private void CheckTransformUpdates(uint _)
+        {
             if (_syncNetworkManager is null || !_networkObject.ShouldSynchronise) return;
+            Writer writer = new(_syncNetworkManager.SerialiserSettings);
 
-            var update = new NetworkUpdate(_transformNetworkID);
             if (_synchronisePosition && _position != transform.position)
             {
-                var position = transform.position;
-                update.Add(0, position);
-                _position = position;
+                _position = transform.position;
+                writer.WriteByte((byte)ETransformFlag.Position);
+                writer.WriteVector3(_position);
             }
             if (_synchroniseRotation && _rotation != transform.rotation)
             {
-                var rotation = transform.rotation;
-                update.Add(1, rotation);
-                _rotation = rotation;
+                _rotation = transform.rotation;
+                writer.WriteByte((byte)ETransformFlag.Rotation);
+                writer.WriteQuaternion(_rotation);
             }
             if (_synchroniseScale && _scale != transform.localScale)
             {
-                var scale = transform.localScale;
-                update.Add(2, scale);
-                _scale = scale;
+                _scale = transform.localScale;
+                writer.WriteByte((byte)ETransformFlag.Scale);
+                writer.WriteVector3(_scale);
             }
 
-            if (update.UpdateValues.Count > 0)
-                _syncNetworkManager.AddNetworkUpdate(update);
-        }
+            if (writer.Length == 0) return;
 
-        private void NetworkIDUpdated()
-        {
-            _syncNetworkManager?.Client.UnregisterByteData(_transformNetworkID, TransformUpdateReceived);
-            _transformNetworkID = $"{_networkObject.NetworkID}#Transform";
-            _syncNetworkManager = _networkObject.SyncNetworkManager;
-            _syncNetworkManager?.Client.RegisterByteData(_transformNetworkID, TransformUpdateReceived);
+            _syncNetworkManager.Client.SendByteDataToAll(_transformNetworkID, writer.GetBuffer(), _synchroniseChannel);
         }
 
         private void TransformUpdateReceived(uint sender, byte[] data)
         {
             if (_syncNetworkManager is null) return;
             
-            var serialiserSettings = _syncNetworkManager.SerialiserSettings;
-            Reader reader = new(data, serialiserSettings);
+            Reader reader = new(data, _syncNetworkManager.SerialiserSettings);
 
             try
             {
                 while (reader.Remaining > 0)
                 {
-                    var flag = reader.ReadByte();
+                    var flag = (ETransformFlag)reader.ReadByte();
                     switch (flag)
                     {
-                        case 0:
+                        case ETransformFlag.Position:
                             _position = transform.position = reader.ReadVector3();
                             break;
-                        case 1:
+                        case ETransformFlag.Rotation:
                             _rotation = transform.rotation = reader.ReadQuaternion();
                             break;
-                        case 2:
+                        case ETransformFlag.Scale:
                             _scale = transform.localScale = reader.ReadVector3();
                             break;
                         default: return; // TODO : handle error
                     }
                 }
-
-                transform.hasChanged = false;
             }
             catch (IndexOutOfRangeException) 
             { 
