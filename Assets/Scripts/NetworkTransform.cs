87def56d4c1f516ad28a8e22dfe8e97be1e49984
jKnepel <knepel.julian@tutanota.com>
2024-07-26 16:46:26 +0200

    chore: add target transform middleware, cleanup

--- Assets/Scripts/NetworkTransform.cs
+++ Assets/Scripts/NetworkTransform.cs
@@ -14,78 +14,6 @@ namespace jKnepel.SynchronisationSchemes
     [AddComponentMenu("SimpleUnityNetworking/Component/Network Transform")]
     public class NetworkTransform : MonoBehaviour
     {
-        private struct ETransformSnapshot
-        {
-            public uint SenderID;
-            public uint Tick;
-            public DateTime Timestamp;
-            public readonly Vector3 Position;
-            public readonly Quaternion Rotation;
-            public readonly Vector3 Scale;
-
-            public ETransformSnapshot(uint senderID, uint tick, DateTime timestamp, Vector3 position, Quaternion rotation, Vector3 scale)
-            {
-                SenderID = senderID;
-                Tick = tick;
-                Timestamp = timestamp;
-                Position = position;
-                Rotation = Quaternion.Normalize(rotation);
-                Scale = scale;
-            }
-        }
-
-        private struct ETransformPacket
-        {
-            public readonly Vector3? Position;
-            public readonly Quaternion? Rotation;
-            public readonly Vector3? Scale;
-
-            public ETransformPacket(Vector3? position, Quaternion? rotation, Vector3? scale)
-            {
-                Position = position;
-                Rotation = rotation;
-                Scale = scale;
-            }
-
-            public static ETransformPacket Read(Reader reader)
-            {
-                var hasPos = reader.ReadBoolean();
-                Vector3? position = null;
-                if (hasPos)
-                    position = reader.ReadVector3();
-
-                var hasRot = reader.ReadBoolean();
-                Quaternion? rotation = null;
-                if (hasRot)
-                    rotation = reader.ReadQuaternion();
-
-                var hasScale = reader.ReadBoolean();
-                Vector3? scale = null;
-                if (hasScale)
-                    scale = reader.ReadVector3();
-
-                return new(position, rotation, scale);
-            }
-
-            public static void Write(Writer writer, ETransformPacket packet)
-            {
-                var hasPos = packet.Position is not null;
-                writer.WriteBoolean(hasPos);
-                if (hasPos)
-                    writer.WriteVector3((Vector3)packet.Position);
-                
-                var hasRot = packet.Rotation is not null;
-                writer.WriteBoolean(hasRot);
-                if (hasRot)
-                    writer.WriteQuaternion((Quaternion)packet.Rotation);
-                
-                var hasScale = packet.Scale is not null;
-                writer.WriteBoolean(hasScale);
-                if (hasScale)
-                    writer.WriteVector3((Vector3)packet.Scale);
-            }
-        }
-        
         #region fields and properties
         
         [SerializeField] private ENetworkChannel _synchroniseChannel = ENetworkChannel.ReliableOrdered;
@@ -107,6 +35,7 @@ namespace jKnepel.SynchronisationSchemes
         private INetworkManager _syncNetworkManager;
 
         private readonly List<ETransformSnapshot> _receivedSnapshots = new();
+        // TODO : delete too long snapshots
         
         #endregion
         
@@ -122,73 +51,75 @@ namespace jKnepel.SynchronisationSchemes
 
         private void Update()
         {
-            if (_networkObject.ShouldSynchronise)
+            if (_networkObject.ShouldSynchronise || _receivedSnapshots.Count == 0)
                 return;
                 
-            if (!_useInterpolation || _receivedSnapshots.Count < 2) return;
-
-            ETransformSnapshot a = default, b = default;
-            var renderingTime = DateTime.Now.AddSeconds(-Mathf.Abs(_interpolationInterval));
-
-            if (_receivedSnapshots.Count == 2)
+            TargetTransform target = default;
+            if (_useInterpolation && _receivedSnapshots.Count >= 2)
             {
-                a = _receivedSnapshots[0];
-                b = _receivedSnapshots[1];
+                var renderingTime = DateTime.Now.AddSeconds(-Mathf.Abs(_interpolationInterval));
+                // TODO : add rate multiplier depending on length of interpolation queue
+                if (_receivedSnapshots.Count == 2)
+                {
+                    target = LinearInterpolateSnapshots(_receivedSnapshots[1], _receivedSnapshots[0], renderingTime);
+                }
+                else
+                {
+                    for (var i = 1; i <= _receivedSnapshots.Count; i++)
+                    {
+                        var snapshot = _receivedSnapshots[^i];
+                        if (snapshot.Timestamp > renderingTime)
+                            continue;
+                        if (i == 1)
+                        {
+                            // TODO : extrapolate?
+                            target = new()
+                            {
+                                Position = snapshot.Position,
+                                Rotation = snapshot.Rotation,
+                                Scale = snapshot.Scale
+                            };
+                        }
+                        else
+                        {
+                            target = LinearInterpolateSnapshots(_receivedSnapshots[^(i - 1)], snapshot, renderingTime);
+                        }
+                        break;
+                    }
+                }
             }
             else
             {
-                for (var i = 2; i <= _receivedSnapshots.Count; i++)
+                var snapshot = _receivedSnapshots[^1];
+                target = new()
                 {
-                    var snapshot = _receivedSnapshots[^i];
-                    if (snapshot.Timestamp > renderingTime)
-                        continue;
-                    b = snapshot;
-                    a = _receivedSnapshots[^(i - 1)];
-                    break;
-                }
-
-                if (a.Tick == 0 || b.Tick == 0)
-                {
-                    // TODO : extrapolate?
-                    a = _receivedSnapshots[^1];
-                    b = _receivedSnapshots[^2];
-                    LinearInterpolateSnapshots(a, b, 1);
-                }
+                    Position = snapshot.Position,
+                    Rotation = snapshot.Rotation,
+                    Scale = snapshot.Scale
+                };
+            }
+            
+            // TODO : add smoothing
+            // TODO : add teleport/snap threshold delta
+            if (_synchronisePosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.Position, Time.deltaTime * _moveMult);
             }
 
-            var t = (float)((renderingTime - b.Timestamp) / (a.Timestamp - b.Timestamp));
-            t = Mathf.Clamp01(t);
-            LinearInterpolateSnapshots(a, b, t);
-            
-            // TODO : add smoothing without interpolation
+            if (_synchroniseRotation)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target.Rotation, Time.deltaTime * _moveMult);
+            }
+
+            if (_synchroniseScale)
+            {
+                transform.localScale = Vector3.MoveTowards(transform.localScale, target.Scale, Time.deltaTime * _moveMult);
+            }
         }
         
         #endregion
         
         #region private methods
-
-        private void LinearInterpolateSnapshots(ETransformSnapshot a, ETransformSnapshot b, float t)
-        {
-            // TODO : add teleport/snap threshold delta
-            // TODO : add rate multiplier depending on length of interpolation queue
-            if (_synchronisePosition)
-            {
-                var targetPos = Vector3.Lerp(a.Position, b.Position, t);
-                transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * _moveMult);
-            }
-
-            if (_synchroniseRotation)
-            {
-                var targetRot = Quaternion.Lerp(a.Rotation, b.Rotation, t);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, Time.deltaTime * _moveMult);
-            }
-
-            if (_synchroniseScale)
-            {
-                var targetScale = Vector3.Lerp(a.Scale, b.Scale, t);
-                transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, Time.deltaTime * _moveMult);
-            }
-        }
 
         private void NetworkIDUpdated()
         {
@@ -248,27 +179,106 @@ namespace jKnepel.SynchronisationSchemes
             }
             catch (IndexOutOfRangeException) { return; }
 
-            if (_useInterpolation)
+            var trf = transform;
+            var snapshot = new ETransformSnapshot(data.SenderID, data.Tick, data.Timestamp, 
+                packet.Position ?? trf.position, 
+                packet.Rotation ?? trf.rotation, 
+                packet.Scale ?? trf.localScale);
+            _receivedSnapshots.Add(snapshot);
+        }
+        
+        private static TargetTransform LinearInterpolateSnapshots(ETransformSnapshot a, ETransformSnapshot b, DateTime time)
+        {
+            var t = (float)((time - b.Timestamp) / (a.Timestamp - b.Timestamp));
+            t = Mathf.Clamp01(t);
+            
+            return new()
             {
-                var trf = transform;
-                var snapshot = new ETransformSnapshot(data.SenderID, data.Tick, data.Timestamp, 
-                    packet.Position ?? trf.position, 
-                    packet.Rotation ?? trf.rotation, 
-                    packet.Scale ?? trf.localScale);
-                _receivedSnapshots.Add(snapshot);
-            }
-            else
-            {
-                var trf = transform;
-                if (packet.Position is not null)
-                    trf.position = (Vector3)packet.Position;
-                if (packet.Rotation is not null)
-                    trf.rotation = (Quaternion)packet.Rotation;
-                if (packet.Scale is not null)
-                    trf.localScale = (Vector3)packet.Scale;
-            }
+                Position = Vector3.Lerp(a.Position, b.Position, t),
+                Rotation = Quaternion.Lerp(a.Rotation, b.Rotation, t),
+                Scale = Vector3.Lerp(a.Scale, b.Scale, t)
+            };
         }
         
         #endregion
+        
+        private struct ETransformSnapshot
+        {
+            public uint SenderID;
+            public uint Tick;
+            public DateTime Timestamp;
+            public readonly Vector3 Position;
+            public readonly Quaternion Rotation;
+            public readonly Vector3 Scale;
+
+            public ETransformSnapshot(uint senderID, uint tick, DateTime timestamp, Vector3 position, Quaternion rotation, Vector3 scale)
+            {
+                SenderID = senderID;
+                Tick = tick;
+                Timestamp = timestamp;
+                Position = position;
+                Rotation = rotation;
+                Scale = scale;
+            }
+        }
+
+        private struct ETransformPacket
+        {
+            public readonly Vector3? Position;
+            public readonly Quaternion? Rotation;
+            public readonly Vector3? Scale;
+
+            public ETransformPacket(Vector3? position, Quaternion? rotation, Vector3? scale)
+            {
+                Position = position;
+                Rotation = rotation;
+                Scale = scale;
+            }
+
+            public static ETransformPacket Read(Reader reader)
+            {
+                var hasPos = reader.ReadBoolean();
+                Vector3? position = null;
+                if (hasPos)
+                    position = reader.ReadVector3();
+
+                var hasRot = reader.ReadBoolean();
+                Quaternion? rotation = null;
+                if (hasRot)
+                    rotation = reader.ReadQuaternion();
+
+                var hasScale = reader.ReadBoolean();
+                Vector3? scale = null;
+                if (hasScale)
+                    scale = reader.ReadVector3();
+
+                return new(position, rotation, scale);
+            }
+
+            public static void Write(Writer writer, ETransformPacket packet)
+            {
+                var hasPos = packet.Position is not null;
+                writer.WriteBoolean(hasPos);
+                if (hasPos)
+                    writer.WriteVector3((Vector3)packet.Position);
+                
+                var hasRot = packet.Rotation is not null;
+                writer.WriteBoolean(hasRot);
+                if (hasRot)
+                    writer.WriteQuaternion((Quaternion)packet.Rotation);
+                
+                var hasScale = packet.Scale is not null;
+                writer.WriteBoolean(hasScale);
+                if (hasScale)
+                    writer.WriteVector3((Vector3)packet.Scale);
+            }
+        }
+
+        public struct TargetTransform
+        {
+            public Vector3 Position;
+            public Quaternion Rotation;
+            public Vector3 Scale;
+        }
     }
 }
